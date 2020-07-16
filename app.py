b2d2e44b050bd40d793c1ba2249b45a4d68431e9
Mac1 <bischi8@gmail.com>
2020-07-16 08:00:57 -0700

    added endpoint

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_bcrypt import Bcrypt
 
 
 app= Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://hchgnlflvayxgp:99524eaa7e709ef8aac17d35157544d91b182ba5d851b427be5cbe3af24b9ab9@ec2-50-17-90-177.compute-1.amazonaws.com:5432/d4ps8s8v21pi0k"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -23,6 +23,7 @@ class User(db.Model):
     lastname = db.Column(db.String(), nullable=False)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(), nullable=False)
+    recipes = db.relationship("Recipe", cascade="all,delete", backref="user", lazy=True)
     
     
 
@@ -44,17 +45,19 @@ class Recipe(db.Model):
     title = db.Column(db.String(), nullable=False)
     ingredients = db.Column(db.String(), nullable=False)
     preperation = db.Column(db.String(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id", nullable=False))
     
     
-    def __init__(self, title, ingredients, preperation):
+    def __init__(self, title, ingredients, preperation, user_id):
         self.title = title
         self.ingredients = ingredients
         self.preperation = preperation
+        self.user_id = user_id
         
         
 class RecipeSchema(ma.Schema):
     class Meta:
-        fields = ("id", "title", "ingredients", "preperation")    
+        fields = ("id", "title", "ingredients", "preperation", "user_id")    
 
 recipe_schema = RecipeSchema()
 recipes_schema = RecipeSchema(many=True)
@@ -89,7 +92,7 @@ def get_users():
 
 @app.route("/user/verified", methods=["POST"])
 def verify_user():
-    print(request.content_type)
+
     if request.content_type != "application/json":
         return jsonify("Error: Data must be sent as JSON")
 
@@ -97,10 +100,15 @@ def verify_user():
     username = post_data.get("username")
     password = post_data.get("password")
 
-    stored_password = db.session.query(User.password).filter(User.username == username).all()
+    stored_password = db.session.query(User.password).filter(User.username == username).first()
     
 
     if stored_password is None:
+        return jsonify("User NOT Verified")
+
+    valid_password_check = bcrypt.check_password_hash(stored_password[0], password)
+
+    if valid_password_check == False:
         return jsonify("User NOT Verified")
 
     return jsonify("User Verified")
@@ -114,10 +122,12 @@ def add_recipe():
     title = post_data.get("title")
     ingredients = post_data.get("ingredients")
     preperation = post_data.get("preperation")
+    username = post_data.get("username")
 
+    user_id = db.session.query(User.id).filter(User.username == username.)first()
     
 
-    new_recipe = Recipe(title, ingredients, preperation)
+    new_recipe = Recipe(title, ingredients, preperation, user_id[0])
     db.session.add(new_recipe)
     db.session.commit()
 
@@ -131,8 +141,8 @@ def get_recipes():
 
 @app.route("/recipe/get/<username>", methods=["GET"])
 def get_recipes_by_username(username):
-    user = db.session.query(User).filter(User.username == username).first()
-    recipes = db.session.query(Recipe).filter(Recipe.user == user).all()
+    user_id = db.session.query(User).filter(User.username == username).first()[0]
+    recipes = db.session.query(Recipe).filter(Recipe.user_id == user_id).all()
     return jsonify(recipes_schema.dump(recipes))
 
 
